5b4688eabe828b43b875d11bdd35bb1c51d5f000
emil1m <marhoffer.emilien@icloud.com>
2026-03-25 19:12:34 +0100

    Add files via upload

---
+++ w4_timing_puzzle.py
@@ -0,0 +1,34 @@
+from expyriment import design, control, stimuli
+
+exp = design.Experiment(name="timing puzzle")
+
+control.set_develop_mode()
+control.initialize(exp)
+
+fixation = stimuli.FixCross()
+text = stimuli.TextLine("Fixation removed")
+
+
+
+
+fixation.present()
+t0 = exp.clock.time
+
+exp.clock.wait(1000)
+
+text.present()
+t1 = exp.clock.time
+fix_duration = (t1 - t0)/1000
+
+exp.clock.wait(1000)
+
+text.present()
+exp.clock.wait(1000)
+
+duration_text = f"Fixation was present on the screen for one second"
+
+text2 = stimuli.TextLine(duration_text)
+text2.present()
+exp.clock.wait(2000)
+
+control.end()
